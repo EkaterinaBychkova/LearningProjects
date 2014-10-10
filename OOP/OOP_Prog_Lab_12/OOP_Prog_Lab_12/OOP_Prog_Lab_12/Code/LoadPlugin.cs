2a030fefdb3b88360ba4c4ca7f275d233867c388
OlegSkomorohov <skom_oleg@hotmail.com>
2014-10-10 11:38:23 +0400

    Change class LoadPlugin.cs Namespace OOP_Prog_Lab_12.Code

--- OOP/OOP_Prog_Lab_12/OOP_Prog_Lab_12/OOP_Prog_Lab_12/Code/LoadPlugin.cs
+++ OOP/OOP_Prog_Lab_12/OOP_Prog_Lab_12/OOP_Prog_Lab_12/Code/LoadPlugin.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 using OOP_Prog_Lab_12.Code.Interfaces;
 
 namespace OOP_Prog_Lab_12.Code
@@ -10,6 +12,54 @@ namespace OOP_Prog_Lab_12.Code
     class LoadPlugin
     {
 
-                
+        private Dictionary<string, IBasicOperations> _pluginsBasicOperations = new Dictionary<string, IBasicOperations>();
+        private Dictionary<string, IOperationsMatrix> _pluginsOperationsMatrix = new Dictionary<string, IOperationsMatrix>();
+        private Dictionary<string, IQueueOperations> _pluginsQueueOperations = new Dictionary<string, IQueueOperations>();
+
+        Assembly mainAssembly;
+
+        public LoadPlugin()
+        {
+
+            mainAssembly = Assembly.GetExecutingAssembly();
+        }
+
+        private void LoadPlugins(string path, string searchInterface, Dictionary<string, IBasicOperations> plugins)
+        {
+
+            plugins.Clear();
+
+            foreach (var itemDll in Directory.GetFiles(path, "*.dll"))
+            {
+
+                Assembly assembly = Assembly.LoadFrom(itemDll);
+
+                foreach (Type type in assembly.GetTypes())
+                {
+                    
+
+                    if (type.GetInterface(searchInterface) == typeof(IBasicOperations))
+                    {
+
+                        //ДАЛЕЕ ЛОГИКА ЗАГРУЗКИ
+                    }
+                }
+            }
+        }
+
+        private void LoadPlugins(string path, string searchInterface, Dictionary<string, IOperationsMatrix> plugins)
+        {
+
+            //ПЕРЕГРУЗКА
+        }
+
+        private void LoadPlugins(string path, string searchInterface, Dictionary<string, IQueueOperations> plugins)
+        {
+
+
+        }
+
+
+
     }
 }
